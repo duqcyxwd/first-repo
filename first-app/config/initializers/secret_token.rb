6b5eb5bca6722dbe4a38dd2f65b22f95c167f561
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-10 08:20:59 -0400

    init rail

--- first-app/config/initializers/secret_token.rb
+++ first-app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'a0460f19e5a9e9287351d844b0cf0715a6c9a7f35f325f037f2772ba6eb59bbd6ec8873fa9a2a6d4702b7b167b349db7d92106d59f86d98bc202197f0833e373'
+FirstApp::Application.config.secret_token = '6d5fefd851b82bea40b9ea450a16f1c5fc31c637fb166f3ba78d871033389c55196b8e81c391f322da46caede14e3de4be9fb002c645bb7f38b05144975f3a87'
