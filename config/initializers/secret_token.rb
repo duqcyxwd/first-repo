fcbb22c9277b3fd96a727491cde37eb31599bee7
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-11 19:22:48 -0400

    add heroku2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6d5fefd851b82bea40b9ea450a16f1c5fc31c637fb166f3ba78d871033389c55196b8e81c391f322da46caede14e3de4be9fb002c645bb7f38b05144975f3a87'
